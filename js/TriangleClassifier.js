014795d21984896727b610686df1e2fee289da7c
Eric Shinn <eric@eric.cx>
2015-05-20 16:16:10 -0400

    Fake the Jasmin test for now

--- js/TriangleClassifier.js
+++ js/TriangleClassifier.js
@@ -2,8 +2,12 @@ var TriangleClassifier = function() {
 
   this.classify = function(a,b,c) {
     // return "scalene";
-    if (a !== b && a!==c && b!==c) return "scalene";
+    if (a !== b && a!==c && b!==c)
+      return "scalene";
+
     // return "isosceles"
+    else
+      return "isosceles";
     for (i=0, iMax=arguments.length; i<iMax; i++) {
       // arguments.
     }
