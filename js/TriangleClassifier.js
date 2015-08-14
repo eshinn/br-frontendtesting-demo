f35bd2ac328825c8d55389285c7322ce79668373
Eric Shinn <eric@eric-shinn.com>
2015-08-14 08:37:32 -0400

    Add extra quote marks that don't need to exist

--- js/TriangleClassifier.js
+++ js/TriangleClassifier.js
@@ -1,11 +1,11 @@
 var TriangleClassifier = function() {
 
   this.classify = function(a,b,c) {
-    // return "scalene";
+//     // return "scalene";
     if (a !== b && a!==c && b!==c)
       return "scalene";
 
-    // return "isosceles"
+//     // return "isosceles"
     else
       return "isosceles";
     for (i=0, iMax=arguments.length; i<iMax; i++) {
